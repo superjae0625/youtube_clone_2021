6284780f912e0de9ce718a78fc4904785b4999fc
jaehyungpark <superjae0625@gmail.com>
2021-05-28 00:18:57 -0600

    Middleware

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const logger = ( req, res, next ) => {
+    console.log( `${ req.method } ${ req.url }` );
+    next();
+};
+
+const privateMiddleware = ( req, res, next ) => {
+    const url = req.url;
+    if ( url === "/protected" ) {
+        return res.send( "<h1>Not Allowed</h1>" );
+    }
+    console.log( "Allowed, you may continue." );
+    next();
+};
+
+const handleHome = ( req, res ) => {
+    return res.send( "I love middlewares" );
+};
+
+
+app.use( logger );
+app.use( privateMiddleware );
+app.get( "/", handleHome );
+
+
+app.listen( PORT, () => console.log( `✅ Server listening on port http://localhost:${ PORT }` ) );
