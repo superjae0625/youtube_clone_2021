ffe2ad4c2b889ba792528d11da99bf3ca6f85553
jaehyungpark <superjae0625@gmail.com>
2021-06-01 00:26:27 -0600

    Router&Controller

--- README.md
+++ README.md
@@ -16,3 +16,18 @@ Cloning Youtube with Vanilla and NodeJS
 - [ ] Edit Video
 
 
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+
+/videos/watch -> Watch video
+/videos/edit -> Edit video
+/videos/delete -> Delete video
+/videos/comments -> Comment on a video
+/videos/comments/delete -> Delete a comment of a video
--- src/server.js
+++ src/server.js
@@ -1,34 +1,17 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
-
 const app = express();
+const logger = morgan( "dev" );
+app.use( logger );
 
-const loggerMiddleWare = morgan( "dev" );
-
-// const logger = ( req, res, next ) => {
-//     console.log( `${ req.method } ${ req.url }` );
-//     next();
-// };
-
-const privateMiddleware = ( req, res, next ) => {
-    const url = req.url;
-    if ( url === "/protected" ) {
-        return res.send( "<h1>Not Allowed</h1>" );
-    }
-    console.log( "Allowed, you may continue." );
-    next();
-};
-
-const handleHome = ( req, res ) => {
-    return res.send( "I love middlewares" );
-};
-
-app.use( loggerMiddleWare );
-// app.use( logger );
-app.use( privateMiddleware );
-app.get( "/", handleHome );
+app.use( "/", globalRouter );
+app.use( "/users", userRouter );
+app.use( "/videos", videoRouter );
 
 
 app.listen( PORT, () => console.log( `✅ Server listening on port http://localhost:${ PORT }` ) );
