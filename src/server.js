dd443c6dabc721f49d83d053f6c79b334ab0ad2c
jaehyungpark <superjae0625@gmail.com>
2021-06-21 06:17:53 -0600

    DB Server Setup

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import "./db";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
@@ -10,9 +11,8 @@ const logger = morgan( "dev" );
 
 app.set( "view engine", "pug" );
 app.set( "views", process.cwd() + "/src/views" );
-
 app.use( logger );
-
+app.use( express.urlencoded( { extended: true } ) );
 app.use( "/", globalRouter );
 app.use( "/users", userRouter );
 app.use( "/videos", videoRouter );
