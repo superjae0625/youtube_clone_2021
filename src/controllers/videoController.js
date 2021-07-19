47536f0814edc3e60f693703ace771fd9b7c51bf
jaehyungpark <superjae0625@gmail.com>
2021-07-19 15:18:41 -0600

    CRUD, mongoDB and mongoose

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,53 +1,55 @@
-let videos = [
-    {
-        title: "First Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id: 1,
-    },
-    {
-        title: "Second Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id: 2,
-    },
-    {
-        title: "Thrid Video",
-        rating: 5,
-        comments: 2,
-        createdAt: "2 minutes ago",
-        views: 59,
-        id: 3,
+import Video from "../models/Video";
+
+export const home = async ( req, res ) => {
+    try {
+        const videos = await Video.find( {} ).sort( { createdAt: "desc" } );
+        return res.render( "home", { pageTitle: "Home", videos } );
+    } catch {
+        return res.render( "server-error" );
     }
-];
-
-export const trending = ( req, res ) => {
-
-    return res.render( "home", { pageTitle: "Home", videos } );
 };
 
-export const watch = ( req, res ) => {
+export const watch = async ( req, res ) => {
     const { id } = req.params;
     // const id = req.params.id;
     // console.log( "Show video", id );
-    const video = videos[ id - 1 ];
-    return res.render( "watch", { pageTitle: `Watching: ${ video.title }`, video } );
+
+    // console.log( id );
+    const video = await Video.findById( id );
+    // console.log( video );
+    if ( video === null ) {
+        return res.render( "404", { pageTitle: "Video not found." } );
+        //not working above 404 code
+    }
+    return res.render( "watch", { pageTitle: video.title, video } );
+
 };
 
-export const getEdit = ( req, res ) => {
+export const getEdit = async ( req, res ) => {
     const { id } = req.params;
-    const video = videos[ id - 1 ];
-    return res.render( "edit", { pageTitle: `Editing: ${ video.title }`, video } );
+    const video = await Video.findById( id );
+    if ( !video ) {
+        return res.render( "404", { pageTitle: "Video not found." } );
+        //not working above 404 code
+    }
+    return res.render( "edit", { pageTitle: `Editing ${ video.title }`, video } );
 };
 
-export const postEdit = ( req, res ) => {
+export const postEdit = async ( req, res ) => {
     const { id } = req.params;
-    const { title } = req.body;
-    videos[ id - 1 ].title = title;
+    const { title, description, hashtags } = req.body;
+    const video = await Video.exists( { _id: id } );
+    if ( !video ) {
+        return res.render( "404", { pageTitle: "Video not found." } );
+        //not working above 404 code
+    }
+    await video.findByIdAndUpdate( id, {
+        title, description, hashtags: hashtags.Video.formathashtags( hashtags )
+    } );
+    // video.title = title;
+    // video.description = description;
+    // video.hashtags = hashtags.split( "," ).map( ( word ) => word.startsWith( '#' ) ? word : `#${ word }` );
+    await video.save();
     return res.redirect( `/videos/${ id }` );
 };
 
@@ -55,25 +57,40 @@ export const getUpload = ( req, res ) => {
     return res.render( "upload", { pageTitle: "Upload Video" } );
 };
 
-export const postUpload = ( req, res ) => {
+export const postUpload = async ( req, res ) => {
     //add a video to the videos array
-    const { title } = req.body;
-    //getting new title from body after change
-    const newVideo = {
-        title,
-        rating: 0,
-        comments: 0,
-        createdAt: "Just now",
-        views: 0,
-        id: videos.length + 1,
-    };
-    videos.push( newVideo );
-    //add new video to the fake database
+    const { title, description, hashtags } = req.body;
+    try {
+        await Video.create( {
+            title,
+            description,
+            hashtags: hashtags
+        } );
+        return res.redirect( "/" );
+    } catch ( error ) {
+        return res.render( "upload", { pageTitle: "Upload Video", errorMessage: error._message } );
+    }
+
+};
+
+export const deleteVideo = async ( req, res ) => {
+    const { id } = req.params;
+    await Video.findByIdAndDelete( id );
+    console.log( id );
+    //delete video
     return res.redirect( "/" );
 };
 
-
-
-
-
-
+export const search = async ( req, res ) => {
+    // console.log( req.query );
+    const { keyword } = req.query;
+    let videos = [];
+    if ( keyword ) {
+        videos = await Video.find( {
+            title: {
+                $regex: new RegExp( keyword, "i" )
+            }
+        } );
+    }
+    return res.render( "search", { pageTitle: "Search", videos } );
+};
